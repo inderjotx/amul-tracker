33b0678f1b5478f4443edc4084ae76502d2587b9
index <inderjotsingh141@gmail.com>
2025-09-06 10:44:09 +0530

    init

---
+++ src/auth/index.ts
@@ -0,0 +1,18 @@
+import {
+    betterAuth
+} from 'better-auth';
+import { db } from '@/server/db';
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { env } from '@/env';
+
+export const auth = betterAuth({
+    socialProviders: {
+        google: {
+            clientId: env.GOOGLE_CLIENT_ID,
+            clientSecret: env.GOOGLE_CLIENT_SECRET
+        }
+    },
+    adapter: drizzleAdapter(db, {
+        provider: "pg",
+    }),
+});
